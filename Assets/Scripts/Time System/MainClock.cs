8123721fdd50345fd4041678c3aa21f037bfb111
Gian Collica <78092119+bulmasbf@users.noreply.github.com>
2021-11-01 23:56:02 +0800

    Started creating the MainClock/Time system, seconds,minutes,hours done -> yet to do day,month,year progression. Sidenote: Slightly over-engineered for this point in the project but it is what it is.

---
+++ Assets/Scripts/Time System/MainClock.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Utility_Classes;
+
+namespace Time_System
+{
+    public class MainClock : MonoBehaviour
+    {
+        public static MainClock Instance;
+        
+        private float _currentSecond;
+        private int _currentMinute;
+        private int _currentHour;
+        private int _currentDay;
+        private int _currentMonth;
+        private int _currentYear;
+        private DateTime _currentDateTime;
+
+        private float _timescaleMultiplier = 75;
+        private int hourToStartDay = 10;
+        private int hoursInDayCycle = 18;
+
+        private bool _isIncrementing = false;
+
+        private void Awake ( )
+        {
+            Instance = this;
+            
+            SetClock ( 10,0,0 );
+            BeginClock (  );
+        }
+
+        private void BeginClock ( )
+        {
+            _isIncrementing = true;
+            IncrementClock (  ).WrapErrors (  );
+        }
+
+        private void PauseClock ( )
+        {
+            _isIncrementing = false;
+        }
+
+        private void SetClock ( int targetHour, int targetMinute, int targetSecond )
+        {
+            _currentSecond = targetSecond;
+            _currentMinute = targetMinute;
+            _currentHour = targetHour;
+        }
+
+        private async Task IncrementClock ( )
+        {
+            while( _isIncrementing )
+            {
+                var secondsElapsed =  Time.deltaTime * _timescaleMultiplier;
+                _currentSecond += secondsElapsed;
+                
+                if( TimeComponentOverflowCheck ( TimeComponent.Minute, _currentSecond ) )
+                {
+                    _currentSecond = TimeComponentOverflowValue ( TimeComponent.Second, _currentSecond );
+                    _currentMinute++;
+                }
+
+                if( TimeComponentOverflowCheck ( TimeComponent.Hour, _currentMinute ) )
+                {
+                    _currentMinute = TimeComponentOverflowValue ( TimeComponent.Minute, _currentMinute );
+                    _currentHour++;
+                }
+
+
+                await UniTask.WaitForEndOfFrame( );
+            }
+        }
+
+        private bool TimeComponentOverflowCheck (TimeComponent componentToCheck, int timeframeElapsed )
+        {
+            return componentToCheck switch
+            {
+                TimeComponent.Minute => timeframeElapsed > 60,
+                TimeComponent.Hour => timeframeElapsed > 60,
+                _ => false
+            };
+        }
+
+        private bool TimeComponentOverflowCheck ( TimeComponent componentToCheck, float timeframeElapsed )
+        {
+            return componentToCheck switch
+            {
+                TimeComponent.Minute => timeframeElapsed > 60,
+                TimeComponent.Hour => timeframeElapsed > 60,
+                _ => false
+            };
+        }
+
+        private int TimeComponentOverflowValue (TimeComponent componentToCheck, int timeframeElapsed )
+        {
+            return componentToCheck switch
+            {
+                TimeComponent.Second => timeframeElapsed / 60,
+                TimeComponent.Minute => timeframeElapsed / 60,
+                TimeComponent.Hour => timeframeElapsed / 60,
+                _ => 0
+            };
+        }
+
+        private float TimeComponentOverflowValue ( TimeComponent componentToCheck, float timeframeElapsed )
+        {
+            return componentToCheck switch
+            {
+                TimeComponent.Second => timeframeElapsed / 60,
+                TimeComponent.Minute => timeframeElapsed / 60,
+                TimeComponent.Hour => timeframeElapsed / 60,
+                _ => 0f
+            };
+        }
+
+        public enum TimeComponent { Second, Minute, Hour };
+
+        public enum DateComponent { Day, Month, Year };
+    }
+}
